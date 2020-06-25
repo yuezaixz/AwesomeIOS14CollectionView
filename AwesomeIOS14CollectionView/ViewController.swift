815551ab5e5a89b6677048813c4d846dbf96752a
yuezaixz <303178394@qq.com>
2020-06-25 12:03:15 +0800

    Simple Setup

--- AwesomeIOS14CollectionView/ViewController.swift
+++ AwesomeIOS14CollectionView/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        var configuration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        configuration.headerMode = UICollectionLayoutListConfiguration.HeaderMode.supplementary
+        let layout = UICollectionViewCompositionalLayout.list(using: configuration)
+        collectionView.collectionViewLayout = layout
     }
-
 
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionView.elementKindSectionHeader {
+            return collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "SimpleSetupHeaderView", for: indexPath)
+        } else {
+            return UICollectionReusableView()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SimpleSetupCell", for: indexPath) as! SimpleSetupCell
+        return cell
+    }
+    
+    
+}
+
+class SimpleSetupCell: UICollectionViewCell {
+    
+    
+}
+
+class SimpleSetupHeaderView: UICollectionReusableView {
+    
+    
+}
